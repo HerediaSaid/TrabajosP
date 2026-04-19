5b6fde6e4e4e0ff742a39abb192479ef785e4190
TP1 <RobertitoHere@gmail.com>
2026-04-19 16:05:41 -0300

    TP1

--- src/TP1/Ejercicio1.java
+++ src/TP1/Ejercicio1.java
@@ -1,4 +1,36 @@
 package TP1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
-}
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int nota;
+
+        System.out.println("--- SISTEMA DE CALIFICACIONES ---");
+        System.out.print("Ingrese la nota del alumno: ");
+
+        if (leer.hasNextInt()) {
+            nota = leer.nextInt();
+
+            if (nota < 1 || nota > 10) {
+                System.out.println("Error: La nota debe estar entre 1 y 10.");
+            } else {
+                verificarCondicion(nota);
+            }
+        } else {
+            System.out.println("Error: No ingresaste un número válido.");
+        }
+
+        leer.close();
+    }
+
+    public static void verificarCondicion(int nota) {
+        if (nota >= 4) {
+            System.out.println("El alumno está aprobado.");
+        } else {
+            System.out.println("El alumno está desaprobado.");
+        }
+    }
+} // Se cierra la clase Ejercicio1
