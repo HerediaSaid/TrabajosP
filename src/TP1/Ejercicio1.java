49c0aa2b0bc60bd8821b67b35ae9a9e8eb864fd7
TP1 <RobertitoHere@gmail.com>
2026-04-19 16:05:41 -0300

    TP2

--- src/TP1/Ejercicio1.java
+++ src/TP1/Ejercicio1.java
@@ -2,7 +2,8 @@ package TP1;
 
 import java.util.Scanner;
 
-public class Ejercicio1 {
+public class
+Ejercicio1 {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
